54210ee196e4f2e3ce870391cd7c16483378ff99
shenjiaqi <shenjiaqi@126.com>
2019-03-12 13:41:39 +0800

    苟富贵

---
+++ scout/src/test/java/com/tongdao/scout/ScoutApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tongdao.scout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
