6fde8d774457003b81c1899188f5ac2ade85147a
shenjiaqi <shenjiaqi@126.com>
2019-03-12 11:32:34 +0800

    第一次提交

---
+++ scout/src/main/java/com/tongdao/scout/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.tongdao.scout.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tongdao.scout.pojo.Demo;
+import com.tongdao.scout.service.DemoService;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	@Autowired
+	private DemoService demoService;
+	@RequestMapping("/hello")
+	public String hello(String from) {
+		return "hello "+from +" returned by server";
+	}
+	@GetMapping(value="/selectById/{id}")
+	public Demo selectById(@PathVariable Integer id, HttpServletRequest request) {
+		return demoService.selectOneById(id);
+	}
+}
---
+++ scout/src/main/java/com/tongdao/scout/pojo/Demo.java
@@ -0,0 +1,65 @@
+package com.tongdao.scout.pojo;
+
+import java.util.Date;
+
+public class Demo {
+    private Integer id;
+
+    private String name;
+
+    private String imgPath;
+
+    private String comm;
+
+    private Date updateTime;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath == null ? null : imgPath.trim();
+    }
+
+    public String getComm() {
+        return comm;
+    }
+
+    public void setComm(String comm) {
+        this.comm = comm == null ? null : comm.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
